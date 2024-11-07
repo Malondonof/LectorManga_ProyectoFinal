12ce7184691e0da645435ee16b148f21e194ccae
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-11-07 07:38:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,20 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 import platform
+
+st.markdown("""
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&family=Lexend:wght@600&display=swap');
+
+    h1, h2, h3 {
+        font-family: 'Lexend', sans-serif;
+    }
+
+    p, div, label, span, input, textarea {
+        font-family: 'Inter', sans-serif;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Título y subtítulo de la aplicación
+st.title('LectorManga')
